bc395d8369d731d21a59314a19ff7b0032c03628
Robert Dunmire III <slackr31337@gmail.com>
2016-04-27 22:25:21 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 ##############################################################################
-config = {
-    "mysql": {
-        "user": "homepi",
-        "pass": "raspberry!",
-        "host": "10.128.2.222",
-        "db": "homepi"
-        "tables": {
-            "users": "tb_users"
+def config():
+    config = {
+        "mysql": {
+            "user": "homepi",
+            "pass": "raspberry!",
+            "host": "10.128.2.222",
+            "db": "homepi"
         }
-
     }
-}
+##############################################################################
