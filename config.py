2a2658cf44f1806241b9564378b46f33c8ab2019
Robert Dunmire III <slackr31337@gmail.com>
2016-04-27 22:20:54 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+##############################################################################
+config = {
+    "mysql": {
+        "user": "homepi",
+        "pass": "raspberry!",
+        "host": "10.128.2.222",
+        "db": "homepi"
+        "tables": {
+            "users": "tb_users"
+        }
+
+    }
+}
